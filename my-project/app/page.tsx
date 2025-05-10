1a3f81dbbfd139b4e7fbe841ddb478be1992ab63
Anh Nguyen <anh.nguyen762005@gmail.com>
2025-05-10 16:19:05 +1000

    feat: dashboard implementation

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,71 +1,21 @@
-import Image from "next/image";
-import { Button } from "./components/ui/button";
+import Bookings from "./components/Bookings";
+import Performance from "./components/Performance";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-row min-h-screen font-[family-name:var(--font-geist-sans)]">
-      {/* Performance */}
-      <div className="flex flex-col px-2 py-7">
-        <h1 className="text-2xl sm:text-xl font-bold text-[var(--foreground)]">Performance</h1>
-        <h3 className="text-lg sm:text-md font-bold text-[var(--foreground)] py-3">Subscribers</h3>
-        <div className="w-[200px] h-[200px] bg-[var(--muted-foreground)]"></div>
-        <div className="py-3">
-          <div className="flex flex-row">
-            <h4 className="text-md font-bold text-[var(--foreground)] grow">Total Subsrcibers</h4>
-            <p className="text-[var(--foreground)]">8000</p>
-          </div>
-          <div className="flex flex-row">
-            <h4 className="text-md font-bold text-[var(--foreground)] grow">New Subscribers</h4>
-            <p className="text-[var(--foreground)]">8000</p>
-          </div>
-          <div className="flex flex-row">
-            <h4 className="text-md font-bold text-[var(--foreground)] grow">Last Subscribers</h4>
-            <p className="text-[var(--foreground)]">8000</p>
-          </div>
+    <div className="flex flex-col lg:flex-row gap-6 p-4 min-h-screen font-[family-name:var(--font-geist-sans)] w-full">
+      <section>
+        <h1 className="text-2xl sm:text-xl font-bold text-foreground">Performance</h1>
+        <div className="w-full max-w-full mt-4">
+          <Performance />
         </div>
-      {/* Last Campaign*/}
-        <h3 className="text-lg sm:text-md font-bold text-[var(--foreground)] py-3">Last Campaign</h3>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Sent</h4>
-          <p className="text-[var(--foreground)]">500</p>
+      </section>
+      <div className="my-2 mx-1 h-px lg:h-auto lg:w-px bg-border flex-shrink-0"></div>      <section className="flex-1 flex flex-col w-full min-h-screen">
+        <h1 className="text-2xl sm:text-xl font-bold text-foreground)">This Week's Bookings</h1>
+        <div className="w-full max-w-full mt-4">
+          <Bookings />
         </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Opened</h4>
-          <p className="text-[var(--foreground)]">400</p>
-        </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Clicked</h4>
-          <p className="text-[var(--foreground)]">100</p>
-        </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">CTR</h4>
-          <p className="text-[var(--foreground)]">25%</p>
-        </div>
-        {/* Last Campaign*/}
-        <h3 className="text-lg sm:text-md font-bold text-[var(--foreground)] py-3">Upcoming Campaigns</h3>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Campaign Name</h4>
-          <p className="text-[var(--foreground)]">Date</p>
-        </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Campaign Name</h4>
-          <p className="text-[var(--foreground)]">Date</p>
-        </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Campaign Name</h4>
-          <p className="text-[var(--foreground)]">Date</p>
-        </div>
-        <div className="flex flex-row">
-          <h4 className="text-md font-bold text-[var(--foreground)] grow">Campaign Name</h4>
-          <p className="text-[var(--foreground)]">Date</p>
-        </div>
-        <Button className="w-full h-10 text-md font-bold mt-5" type="submit">
-          Create Campaign
-        </Button>
-      </div>
-      <div className="flex flex-col px-2 py-7">
-        <h1 className="text-2xl sm:text-xl font-bold text-[var(--foreground)]">This Week's Bookings</h1>
-      </div>
+      </section>
     </div>
   );
 }
